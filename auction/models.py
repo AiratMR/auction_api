c59f3df900ac6e3e55532497df51843c769f69e6
Айрат Мухаметгараев <airat9753@gmail.com>
2021-01-24 18:27:33 +0300

    Добавил начальную миграцию моделей Auction и AuctionBid

--- auction/models.py
+++ auction/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Auction(models.Model):
+    """
+    Auction model
+
+    Attributes:
+        creator (User): auction creator
+        name (str): goods name
+        description (str): goods description
+        starting_price (float): price at which trading starts
+        price_step (float): minimum price step to increase the bid
+        creation_date (datetime): auction creation time
+        expiration_date (datetime): auction expiration time
+        is_closed (bool): auction is closed
+    """
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    starting_price = models.FloatField()
+    price_step = models.FloatField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateTimeField()
+    is_closed = models.BooleanField(default=False)
+
+
+class AuctionBid(models.Model):
+    """
+    Auction rate
+
+    Attributes:
+        user (User): bid creator
+        auction (Auction): auction
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    price = models.FloatField()
+    creation_date = models.DateTimeField(auto_now_add=True)
