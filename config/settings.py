bb4764bc63a2e402ccafff30db3e92ee37069b86
Айрат Мухаметгараев <airat9753@gmail.com>
2021-01-27 23:35:19 +0300

    ALLOWED_HOSTS = ['*']

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'x5^uqu$!t=e5&c&og&+(11kbcrag0-8(aoqyx2#t%m97hqmnfx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
